287e50d4ffb7657a751a78453f28efef6ab18b88
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-03-17 14:57:40 -0500

    added to js

--- script.js
+++ script.js
@@ -47,6 +47,46 @@ var getWeatherApi = function() {
       tempList.appendChild(listItem);
     }
   });
+/* a way to dispay weather */
+  var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+      repoContainerEl.textContent = 'No repositories found.';
+      return;
+    }
+  
+    repoSearchTerm.textContent = searchTerm;
+  
+    for (var i = 0; i < repos.length; i++) {
+      var repoName = repos[i].owner.login + '/' + repos[i].name;
+  
+      var repoEl = document.createElement('a');
+      repoEl.classList = 'list-item flex-row justify-space-between align-center';
+      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+  
+      var titleEl = document.createElement('span');
+      titleEl.textContent = repoName;
+  
+      repoEl.appendChild(titleEl);
+  
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+  
+      repoEl.appendChild(statusEl);
+  
+      repoContainerEl.appendChild(repoEl);
+  
+
+
+
+
+
  /*  create a save to local storage  */
  var saveData = function() {
   localStorage.setItem("weather", JSON.stringify(tasks));
